18a5f90f5441ec3f8ce3f98f8d7b6024cba6d075
unknown <steven.huertas@labinfo.is.escuelaing.edu.co>
2024-03-05 17:26:52 -0500

    final

---
+++ src/main/java/edu/escuelaing/arem/ASE/app/LogService.java
@@ -0,0 +1,20 @@
+package edu.escuelaing.arem.ASE.app;
+
+import java.rmi.server.RemoteServer;
+
+import static spark.Spark.staticFiles;
+import static spark.Spark.get;
+
+public class LogService {
+    public static void main(String[] args) {
+
+        RemoteLogServerInvoke remoteLogServerInvoke = new RemoteLogServerInvoke(); // Crear una instancia de RemoteLogServerInvoke
+        // root is 'src/main/resources', so put files in 'src/main/resources/public'
+        staticFiles.location("/public"); // Static files
+
+        get("/logservicefacade", (req, res) -> {
+            res.type("application/json");
+            return "{\"login1\":\"20-2-2024-log initial\"}";
+        });
+    }
+}
